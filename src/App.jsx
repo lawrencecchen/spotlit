8b5d3338e6981f67a1a1b464fa2869c5a842e8c6
Lawrence Chen <lawrencechen2002@gmail.com>
2022-02-13 12:23:38 -0800

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,62 @@
-import { onCleanup, onMount, mergeProps, createSignal } from 'solid-js';
-import babelParser from '@babel/parser';
+import {
+  onCleanup,
+  onMount,
+  mergeProps,
+  createSignal,
+  createMemo,
+  createResource,
+  Show,
+  Suspense,
+} from "solid-js";
+import { parse } from "@babel/parser";
+import { transform } from "@babel/standalone";
+import { createEffect } from "solid-js";
+import { render } from "solid-js/web";
 
-console.log(babelParser);
+// @ts-ignore
+// import babelPresetSolid from "babel-preset-solid";
+
+const SOLID_VERSION = "1.3.6";
+
+let cachedPreset;
+
+async function loadBabel(solidVersion = SOLID_VERSION) {
+  let solid;
+
+  try {
+    // const preset =
+    //   solidVersion === SOLID_VERSION
+    //     ? await Promise.resolve({ default: babelPresetSolid })
+    //     : await import(
+    //         /* @vite-ignore */ `https://esm.sh/babel-preset-solid@${solidVersion}`
+    //       );
+    const preset =
+      cachedPreset ||
+      (await import(
+        /* @vite-ignore */ `https://esm.sh/babel-preset-solid@${solidVersion}`
+      ));
+    // const preset = await Promise.resolve({ default: babelPresetSolid });
+
+    if (!cachedPreset) {
+      cachedPreset = preset;
+    }
+
+    solid = preset.default;
+  } catch {
+    // solid = babelPresetSolid;
+  }
+
+  const babel = (code, opts = { babel: {}, solid: {} }) =>
+    transform(code, {
+      presets: [
+        [solid, { ...opts.solid }],
+        ["typescript", { onlyRemoveTypeImports: true }],
+      ],
+      ...opts.babel,
+    });
+
+  return babel;
+}
 
 function createDragOffset(el, accessor) {
   function mousemove(e) {
@@ -15,21 +70,21 @@ function createDragOffset(el, accessor) {
   }
 
   function mousedown(e) {
-    document.addEventListener('mousemove', mousemove);
+    document.addEventListener("mousemove", mousemove);
   }
 
   function mouseup() {
-    document.removeEventListener('mousemove', mousemove);
+    document.removeEventListener("mousemove", mousemove);
   }
 
   onMount(() => {
-    el.addEventListener('mousedown', mousedown);
-    document.addEventListener('mouseup', mouseup);
+    el.addEventListener("mousedown", mousedown);
+    document.addEventListener("mouseup", mouseup);
   });
 
   onCleanup(() => {
-    el.removeEventListener('mousedown', mousedown);
-    document.removeEventListener('mouseup', mouseup);
+    el.removeEventListener("mousedown", mousedown);
+    document.removeEventListener("mouseup", mouseup);
   });
 }
 
@@ -40,18 +95,21 @@ function InputSlider(props) {
 
   function bound(value, min, max) {
     if (value > max) {
-      console.log('max');
       return max;
     } else if (value < min) {
-      console.log('min');
       return min;
     }
     return value;
   }
 
-  function submit(e) {
-    e.preventDefault();
+  function submit() {
     merged.onInput(bound(value(), merged.min, merged.max));
+  }
+
+  function keydown(e) {
+    if (e.key === "Enter") {
+      submit();
+    }
   }
 
   return (
@@ -66,16 +124,16 @@ function InputSlider(props) {
       >
         {merged.label}
       </div>
-      <form onSubmit={submit} class="m-0">
-        <input
-          class="pr-3 tabular-nums py-1.5 outline-none cursor-default"
-          type="text"
-          onClick={() => inputRef.select()}
-          onInput={(e) => setValue(+e.target.value)}
-          ref={inputRef}
-          value={merged.value}
-        />
-      </form>
+      <input
+        class="pr-3 tabular-nums py-1.5 outline-none cursor-default w-auto"
+        type="text"
+        onClick={() => inputRef.select()}
+        onInput={(e) => setValue(+e.currentTarget.value)}
+        onBlur={submit}
+        ref={inputRef}
+        value={merged.value}
+        onKeyDown={keydown}
+      />
     </div>
   );
 }
@@ -90,27 +148,74 @@ function NumberInputSlider(props) {
   );
 }
 
-function App() {
+function Editor(props) {
   const [fontSize, setFontSize] = createSignal(10);
-  const [code, setCode] = createSignal('<div>hi</div>');
+  const code = createMemo(
+    () =>
+      `import { render } from 'solid-js/web'; function MyComponent() { return <div style="font-size: ${fontSize()}">Hello World</div> } export default function Preview() { return <MyComponent /> } render(<Preview />, document.getElementById('preview-area')); console.log(document.getElementById('preview-area'));`
+  );
+  const ast = createMemo(() =>
+    parse(code(), { plugins: ["jsx"], sourceType: "module" })
+  );
+  const compiled = createMemo(() => {
+    const result = props.babel(code(), {
+      solid: {},
+      babel: { filename: "test.tsx" },
+    });
+    return result.code;
+  });
+
+  createEffect(() => {
+    // render(, previewRef)
+  });
+
+  let previewRef;
 
   return (
-    <div class="flex h-full">
-      <div class="p-4 flex-1">
-        <div class="border p-3" innerHTML={code()}></div>
-        <pre class="text-xs mt-2">{code()}</pre>
+    <div class="flex h-full max-w-screen overflow-hidden">
+      <div class="p-4 grow overflow-hidden min-w-0">
+        <div class="border p-3" ref={previewRef} id="preview-area">
+          {/* {createRoot(() =>)} */}
+        </div>
+        <script innerHTML={compiled()}></script>
+        <script>console.log('heyo')</script>
+        {/* <div class="border p-3" innerHTML={code()}></div> */}
+
+        <div className="text-xs mt-4 space-y-3">
+          <p>input</p>
+          <pre class="mt-2 p-3 border overflow-scroll">{code()}</pre>
+          <p>output</p>
+          <pre class="mt-2 p-3 border overflow-scroll">{compiled()}</pre>
+        </div>
       </div>
-      
-      <div class="h-full border-l">
+
+      <div class="h-full border-l max-w-[240px] w-full">
         <div class="p-4">
-          <NumberInputSlider
-            label="H"
-            value={fontSize()}
-            onInput={(v) => setFontSize(v)}
-          />
+          <div className="flex">
+            <NumberInputSlider
+              label="Font"
+              value={fontSize()}
+              onInput={(v) => setFontSize(v)}
+            />
+            {/* <NumberInputSlider
+              label="W"
+              value={fontSize()}
+              onInput={(v) => setFontSize(v)}
+            /> */}
+          </div>
         </div>
       </div>
     </div>
+  );
+}
+
+function App() {
+  const [babel] = createResource(() => loadBabel());
+
+  return (
+    <Show when={babel()} fallback="loading...">
+      <Editor babel={babel()} />
+    </Show>
   );
 }
 
